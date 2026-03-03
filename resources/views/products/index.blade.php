9dc59a24aef07c62f5e1bd3f3ef564dbb6f818d3
PlaysJr <jorgegael25@hotmail.com>
2026-03-02 21:24:36 -0700

    homework 5

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,5 +4,12 @@
     <title>Product View</title>
 </head>
 <body>
+
+    <h1>Products</h1>
+
+    <a href="{{ route('products.create') }}">
+        <button>Add Product</button>
+    </a>
+
 </body>
 </html>
